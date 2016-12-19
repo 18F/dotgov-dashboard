5159d6f06352e05fd3ffd105f819f5dbc9686d98
Eric Mill <eric.mill@gsa.gov>
2016-12-19 09:13:18 -0500

    don't run inspect anymore

--- data/update.py
+++ data/update.py
@@ -51,7 +51,7 @@ BUCKET_NAME = "pulse.cio.gov"
 # domain-scan information
 SCAN_TARGET = os.path.join(this_dir, "./output/scan")
 SCAN_COMMAND = os.environ.get("DOMAIN_SCAN_PATH", None)
-SCANNERS = os.environ.get("SCANNERS", "pshtt,analytics,sslyze,inspect,tls")
+SCANNERS = os.environ.get("SCANNERS", "pshtt,analytics,sslyze,tls")
 ANALYTICS_URL = os.environ.get("ANALYTICS_URL", META["data"]["analytics_url"])
 
 # subdomain gathering/scanning information
